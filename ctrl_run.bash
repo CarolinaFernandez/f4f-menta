3edf4c3917c48704929e8820e9382c830a61e6d5
Carolina Fernandez <carolina.fernandez@i2cat.net>
2016-05-02 18:14:40 +0200

    Custom Ryu simple switch app with timeouts=30sec

--- ctrl_run.bash
+++ ctrl_run.bash
@@ -2,4 +2,4 @@
 
 RYU_PATH=~/f4f_menta/ryu
 
-nohup python $RYU_PATH/bin/ryu-manager --verbose --observe-links --app-lists $RYU_PATH/ryu/app/ofctl_rest.py $RYU_PATH/ryu/app/simple_switch_f4f_menta.py &
+nohup python $RYU_PATH/bin/ryu-manager --verbose --observe-links --app-lists $RYU_PATH/ryu/app/ofctl_rest.py ryu-app/simple_switch_f4f_menta.py &
